90ae47c06a517eda30ddc3ff21418ede5f2abd1e
Ekapot Suangmon <ake.ekapot@rabbitstale.com>
2019-07-22 20:09:46 +0700

    init

---
+++ Assets/js/components/AsgardPermissions.vue
@@ -0,0 +1,149 @@
+<template>
+    <div>
+        <p class="pull-right">
+            <el-button type="text"
+                       @click="changeStateForAll(1)">
+                {{ trans('roles.allow all') }}
+            </el-button>
+            <el-button type="text"
+                       :disabled="isRole"
+                       @click="changeStateForAll(0)">
+                {{ trans('roles.inherit all') }}
+            </el-button>
+            <el-button type="text"
+                       @click="changeStateForAll(-1)">
+                {{ trans('roles.deny all') }}
+            </el-button>
+        </p>
+        <div v-for="(value, name) in allPermissions"
+            :key="name"
+            v-bind:class="[ hiddenModule.indexOf(name) != -1 ? 'hidden' : '' ]"
+        >
+            <h3>{{ name }}</h3>
+            <div v-for="(permissionActions, subPermissionTitle) in value" :key="subPermissionTitle">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <!-- <h4 class="pull-left">{{ trans('roles.module.'+subPermissionTitle) }}</h4> -->
+                                <h4 class="pull-left">{{ subPermissionTitle }}</h4>
+                            </div>
+                            <div class="col-md-9">
+                                <p  style="margin-top: 10px;">
+                                    <el-button type="text"
+                                               @click="changeState(subPermissionTitle, permissionActions, 1)">
+                                        {{ trans('roles.allow all') }}
+                                    </el-button>
+                                    <el-button type="text"
+                                               :disabled="isRole"
+                                               @click="changeState(subPermissionTitle, permissionActions, 0)">
+                                        {{ trans('roles.inherit all') }}
+                                    </el-button>
+                                    <el-button type="text"
+                                               @click="changeState(subPermissionTitle, permissionActions, -1)">
+                                        {{ trans('roles.deny all') }}
+                                    </el-button>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12" v-for="(label, permissionAction) in permissionActions" :key="permissionAction">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="visible-sm-block visible-md-block visible-lg-block">
+                                    <label class="control-label text-right" style="display: block">{{ parseTranslation(label) }}</label>
+                                </div>
+                                <div class="visible-xs-block">
+                                    <label class="control-label">{{ parseTranslation(label) }}</label>
+                                </div>
+                            </div>
+                            <div class="col-md-9">
+                                <el-radio-group v-model="permissions[`${subPermissionTitle}.${permissionAction}`]">
+                                    <el-radio-button :label="1" @click="triggerEvent">{{ trans('roles.allow') }}</el-radio-button>
+                                    <el-radio-button :label="0" @click="triggerEvent" :disabled="isRole">{{ trans('roles.inherit') }}</el-radio-button>
+                                    <el-radio-button :label="-1" @click="triggerEvent">{{ trans('roles.deny') }}</el-radio-button>
+                                </el-radio-group>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import StringHelpers from '../../../../Core/Assets/js/mixins/StringHelpers.vue';
+
+    export default {
+        mixins: [StringHelpers],
+        props: {
+            isRole: { type: Boolean },
+            currentPermissions: { default: null },
+        },
+        data() {
+            return {
+                permissions: {},
+                allPermissions: {},
+                hiddenModule: ['Workflow','Workshop','Translation','Tag','Setting','Page','Menu','Media','Core','Dashboard','History'],
+            };
+        },
+        methods: {
+            triggerEvent() {
+                this.$emit('input', this.permissions);
+            },
+            parseTranslation(label) {
+                return this.trans(label.split('::')[1]);
+            },
+            getPermissionKey(subPermissionTitle, permissionAction) {
+                return `${subPermissionTitle}.${permissionAction}`;
+            },
+            changeState(permissionPart, actions, state) {
+                _.forEach(actions, (translationKey, key) => {
+                    this.permissions[`${permissionPart}.${key}`] = state;
+                });
+            },
+            changeStateForAll(state) {
+                _.forEach(this.permissions, (index, permission) => {
+                    this.permissions[permission] = state;
+                });
+            },
+            fetchPermissions() {
+                axios.get(route('api.user.permissions.index'))
+                    .then((response) => {
+                        this.loading = false;
+                        this.allPermissions = response.data.permissions;
+                    });
+            },
+            setDefaultPermission() {
+                // console.log(3)
+                for(var name in this.allPermissions) {
+                    if(this.hiddenModule.indexOf(name) != -1){
+                        for(var subPermissionTitle in this.allPermissions[name]) {
+                            for(var permissionTitle in this.allPermissions[name][subPermissionTitle]) {
+                                this.permissions[subPermissionTitle+'.'+permissionTitle] = 1;
+                            }
+                            this.changeState(subPermissionTitle, this.allPermissions[name][subPermissionTitle], 1)
+                        }
+                    }
+                }
+            }
+        },
+        watch: {
+            currentPermissions() {
+                if (this.currentPermissions !== null) {
+                    this.permissions = this.currentPermissions;
+                }
+            },
+            allPermissions() {
+                this.setDefaultPermission()
+            }
+        },
+        mounted() {
+            this.fetchPermissions();
+        },
+    };
+</script>
